fbd9eee6efdf609ed81c3e083c83663dd26b6eb8
Serhii Honchar <sehonc@softserveinc.com>
2022-01-28 12:59:11 +0200

    add docker configuration

--- customer/src/main/java/com/serhiihonchar/customer/CustomerApplication.java
+++ customer/src/main/java/com/serhiihonchar/customer/CustomerApplication.java
@@ -2,8 +2,11 @@ package com.serhiihonchar.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
--- customer/src/main/java/com/serhiihonchar/customer/CustomerService.java
+++ customer/src/main/java/com/serhiihonchar/customer/CustomerService.java
@@ -1,6 +1,9 @@
 package com.serhiihonchar.customer;
 
-public record CustomerService() {
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -9,6 +12,8 @@ public record CustomerService() {
                 .build();
         //todo: check if email valid
         //todo: check if email not taken
-        //todo: store customer in db
+
+        //store customer in db
+        customerRepository.save(customer);
     }
 }
